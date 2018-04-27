8e85cc9edbea74e60332088ac28ce8987d57270d
paulVer777 <pawel.gitara@onet.eu>
2018-04-27 09:59:24 +0200

    cykl zycia komponentów

--- src/components/CounterWithState.js
+++ src/components/CounterWithState.js
@@ -9,16 +9,40 @@ const styles = {
 
 class CounterWithState extends React.Component {
     // THIS IS ES6
-    constructor(){
-        super()
+    constructor() {
+
+       console.log('component is created');
+        super();
 
         this.state = {
             count: 0
-        }
+        };
 
         this.minusBtnClick = () => this.setState({count: this.state.count - 1})
         this.plusBtnClick = () => this.setState({count: this.state.count + 1})
     }
+
+
+    componentWillMount() {
+
+        console.log('Counter will be mounted')
+
+    }
+
+
+    componentDidMount() {
+
+        console.log('Counter is mounted')
+
+    }
+
+
+    componentWillUnmount() {
+
+        console.log('Counter will be unmounted')
+
+    }
+
 
     // USING CLASS PROPERTIES - STAGE-2 - THIS IS NOT IN ES6 !!!
     // state = {
@@ -29,6 +53,8 @@ class CounterWithState extends React.Component {
     // plusBtnClick = () => this.setState({count: this.state.count + 1})
 
     render() {
+
+        console.log('counter renders');
         return (
             <div>
                 <h1>{this.state.count}</h1>
